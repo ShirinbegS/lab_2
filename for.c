01ad468f5bbbb3d980e0a9a4a8874d9211d014c0
root <root@LAPTOP-SG2OVKS4>
2024-10-17 09:41:17 +0500

    lab2

--- for.c
+++ for.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    double h, x=0;
+    double h;
     FILE *graph;
-    printf("Введите h:");
+    printf("Enter h: ");
     scanf("%lf", &h);
-    graph = fopen("my_graph.txt", "w");
-    while(x<=2+(h/2)){
+    graph = fopen("graph.txt", "w");
+    for (double x=0; x<=2+h/2; x=x+h){
         if(x<=1){
             fprintf(graph, "%lf\t%lf\n", x, cos(x+pow(x,3)));
             printf("%lf\t%lf\n", x, cos(x+pow(x,3)));
@@ -14,8 +14,8 @@ int main(){
             fprintf(graph, "%lf\t%lf\n", x, exp(pow(-x,2)) -pow(x,2) + 2*x);
             printf("%lf\t%lf\n", x, exp(pow(-x,2)) -pow(x,2) + 2*x);
         }
-        x+=h;
     }
     fclose(graph);
     return 0;
-}  
+        
+}
